52c74c16c2951beb82136c255704410ec72ebd3e
Polqt <poyhidalgo@gmail.com>
2025-03-28 00:35:10 +0800

    chore: Implement movie creation and retrieval functionality in the CRUD API

--- movies-crud/main.go
+++ movies-crud/main.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"golang.org/x/exp/rand"
 )
 
 type Movie struct {
@@ -31,14 +32,36 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 
 func getMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := 
+	params := mux.Vars(r)
+
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
 }
 
 func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewDecoder(w).Encode(movie)
 }
 
 func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
 
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			
+		}
+	}
 }
 
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
@@ -47,9 +70,12 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
 
 	for index, item := range movies {
 		if item.ID == params["id"] {
-			movies = append(movies[:index], movies)
+			movies = append(movies[:index], movies[index+1:]...)
+			break
 		}
 	}
+
+	json.NewEncoder(w).Encode(movies)
 }
 
 func main() {
